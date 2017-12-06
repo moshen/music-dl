476efb72939f07c7079bd3b0f8653d41abd87e24
Colin Kennedy <moshen.colin@gmail.com>
2017-12-05 23:45:05 -0500

    Add appveyor config

--- build.py
+++ build.py
@@ -6,6 +6,10 @@ from zipfile import ZipFile
 from lxml import etree
 
 def windowsBuild():
+    pyinstaller = 'pyinstaller'
+    if os.environ['PYINSTALLER_PATH'] != None:
+        pyinstaller = os.environ['PYINSTALLER_PATH']
+
     ffmpegPageReq = requests.get('http://ffmpeg.zeranoe.com/builds/')
     html = etree.HTML(ffmpegPageReq.text)
     ffmpegVersion = html.xpath('//label[contains(@title, "Nightly")]/input')[0].attrib['value']
@@ -13,7 +17,7 @@ def windowsBuild():
     ffmpegPath = 'ffmpeg-%s-win64-static/bin/ffmpeg.exe' % ffmpegVersion
     ZipFile(BytesIO(ffmpegZipReq.content)).extract(ffmpegPath)
     os.rename(ffmpegPath, 'ffmpeg.exe')
-    run('pyinstaller -F -w --add-binary "ffmpeg.exe;ffmpeg.exe" music-dl.py', shell = True)
+    run('%s -F -w --add-binary "ffmpeg.exe;ffmpeg.exe" music-dl.py' % pyinstaller, shell = True)
 
 if os.name == 'nt':
     windowsBuild()
